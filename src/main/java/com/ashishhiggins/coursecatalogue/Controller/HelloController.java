36c585d9408e754e528423720f3048499265f814
ASHISH HIGGINS <higginsashish@icloud.com>
2024-11-15 19:43:34 +0530

    final commit

--- src/main/java/com/ashishhiggins/coursecatalogue/Controller/HelloController.java
+++ src/main/java/com/ashishhiggins/coursecatalogue/Controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.ashishhiggins.coursecatalogue.Controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class gitHelloController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+    public String sayHello(Authentication authentication) {
+       Jwt jwt = (Jwt) authentication.getPrincipal();
+        return "Hello, World! "+ jwt.getClaims();
     }
 }
