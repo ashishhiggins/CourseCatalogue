5ac90d723465f6c6124fb81a14563559404c1ef5
ASHISH HIGGINS <98216596+ashishhiggins@users.noreply.github.com>
2024-11-16 15:08:25 +0530

    Update HelloController.java

--- src/main/java/com/ashishhiggins/coursecatalogue/Controller/HelloController.java
+++ src/main/java/com/ashishhiggins/coursecatalogue/Controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.ashishhiggins.coursecatalogue.Controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.jwt.Jwt;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello(Authentication authentication) {
-       Jwt jwt = (Jwt) authentication.getPrincipal();
-        return "Hello, World! "+ jwt.getClaims();
+    public String sayHello() {
+     
+        return "Hello, World! ";
     }
 }
